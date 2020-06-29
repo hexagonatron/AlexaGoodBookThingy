76065b024de524d3d21c7de472075226b800ab6e
Hexagonatron <46476247+hexagonatron@users.noreply.github.com>
2020-06-29 16:06:54 +0930

    run scripts

---
+++ alexa/create_screen.sh
@@ -0,0 +1,5 @@
+#!/usr/local/bin/bash
+
+cd /scripts/AlexaGoodBookThingy/alexa
+
+screen -dmS AlexaGoodBookThingy ./run_script.sh
---
+++ alexa/run_script.sh
@@ -0,0 +1,5 @@
+#!/usr/local/bin/bash
+
+cd /scripts/AlexaGoodBookThingy/alexa
+
+/usr/local/bin/node /scripts/AlexaGoodBookThingy/alexa/app.js
